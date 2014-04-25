1b78871182c85cd9b73fa624b7f4252c0ad74001
Joriek Rogge <Rogge.j@telenet.be>
2014-04-25 09:58:39 +0200

    made special file for events

---
+++ evenementen.php
@@ -0,0 +1,8 @@
+<?php 
+	include_once("lib/event.php");
+$events = [	new Event('1', 'Event 1: Lorem ipsum dolor sit amet', 'img1.jpg', 'discription', 'date', 'time', 'website', 'email'),
+				new Event('2', 'Event 2: Consectetur adipisicing elit', 'img2.jpg', 'discription', 'date', 'time', 'website', 'email'),
+				new Event('3','Event 3: Sed do eiusmod ', 'img3.jpg', 'discription', 'date', 'time', 'website', 'email'),
+				new Event('4','Event 4: Tempor incididunt', 'img4.jpg', 'discription', 'date', 'time', 'website', 'email'),
+				new Event('5','Event 5: Dolore magna aliqua', 'img5.jpg', 'discription', 'date', 'time', 'website', 'email')];
+				?>
