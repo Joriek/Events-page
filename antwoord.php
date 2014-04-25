222b7d5aa55e13057213b8f2b8bb399f83fec754
Joriek Rogge <Rogge.j@telenet.be>
2014-04-25 09:59:01 +0200

    special file for respons page

---
+++ antwoord.php
@@ -0,0 +1,17 @@
+<?php 
+	include_once("evenementen.php");
+	var_dump($events);
+
+	$content = "";
+
+ ?>
+
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<title></title>
+ </head>
+ <body>
+  <?= $content ?>
+ </body>
+ </html>
