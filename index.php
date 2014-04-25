2cde949cdc0c988f813f3b8893a14e651467df7c
Joriek Rogge <Rogge.j@telenet.be>
2014-04-25 09:58:21 +0200

    fully works

--- index.php
+++ index.php
@@ -1,26 +1,20 @@
 <?php
-
 	include_once("lib/html.php");
-	include_once("lib/event.php");
+	include_once("evenementen.php");
+	
 
 	$imageSize = ["height" => 100, "width" => 100];
-
-
-	$events = [	new Event('Event 1: Lorem ipsum dolor sit amet', 'img1.png', 'discription', 'date', 'time', 'website', 'email'),
-				new Event('Event 2: Consectetur adipisicing elit', 'img2.jpg', 'discription', 'date', 'time', 'website', 'email'),
-				new Event('Event 3: Sed do eiusmod ', 'img3.jpg', 'discription', 'date', 'time', 'website', 'email'),
-				new Event('Event 4: Tempor incididunt', 'img4.jpg', 'discription', 'date', 'time', 'website', 'email'),
-				new Event('Event 5: Dolore magna aliqua', 'img5.jpg', 'discription', 'date', 'time', 'website', 'email')];
 
 	$content = "";
 
 	foreach ($events as $event) {
 		$content .= new Div(
+			new image($event->image, $imageSize).
 			new Link(
 				new Heading($event->title),
 				['href'=>'antwoord.php']
-			).
-			new image($event->image, $imageSize));
+			)
+			);
 	}
 
 		
@@ -35,6 +29,6 @@
 	<link rel="stylesheet" type="text/css" href="css/signup.css">
 </head>
 <body>
-	<?= $content ?>
+	<div class = "container"><?php echo $content ?></div>
 </body>
 </html>
