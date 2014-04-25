cb3505b6d2c67c9653e06c9c994d122e37b35d20
Joriek Rogge <Rogge.j@telenet.be>
2014-04-25 10:23:42 +0200

    fully works

--- antwoord.php
+++ antwoord.php
@@ -1,8 +1,26 @@
 <?php 
-	include_once("evenementen.php");
-	var_dump($events);
+	include_once("lib/html.php");
+	include("evenementen.php");
 
+	$event =  null;
 	$content = "";
+
+$id = $_GET["id"] ;
+		foreach ($events as $ev) { 
+			if ($id == $ev->id ) {
+				$event = $ev;
+			}
+		}
+
+		$content .= new Div(
+
+				new Heading($event->title) .
+				new Heading($event->description) .
+				new Heading($event->date) . 
+				new Heading($event->time) .
+				new Heading($event->website) . 
+				new Heading($event->email)
+				);
 
  ?>
 
