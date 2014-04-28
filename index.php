34b0bcba1dce3879ffee8021dc3229b72f8f50ba
Joriek Rogge <Rogge.j@telenet.be>
2014-04-28 09:59:41 +0200

    added code to add events

--- index.php
+++ index.php
@@ -1,23 +1,35 @@
 <?php
 	include_once("lib/html.php");
 	include_once("evenementen.php");
+	include_once("openDB.php");
+	$links="";
+
+	while($row = $STH->fetch()){
+		$id = $row['id'];
+		$links=$links.new Link($row['Name'],array("class"=>"list-group-item","href"=>"antwoord.php?event=$id"))."\n";
+	}
 	
 
 	$imageSize = ["height" => 100, "width" => 100];
 
 	$content = "";
 
-	foreach ($events as $event) {
-		$content .= new Div(
-			new image($event->image, $imageSize).
-			new Link(
-				new Heading($event->title),
-				['href'=>'antwoord.php?id='.$event->id]
-			)
-			);
-	}
+//	foreach ($events as $event) {
+//		$content .= new Div(
+//			new image($event->image, $imageSize).
+//			new Link(
+//				new Heading($event->title),['href'=>'antwoord.php?id='.$event->id]
+//				)
+//			
+//			);
+//	}
+//	$content .= new Link("New Event",array("href"=>"newevent.php","class"=>"btn btn-primary btn-lg active","role"=>"button"));
 
-		
+	$content = 	new Heading("Events",1).
+			$links.
+			new Paragraph("").
+			new Link("New Event",array("href"=>"newevent.php","class"=>"btn btn-primary btn-lg active","role"=>"button"));
+
 	
 ?>
 
