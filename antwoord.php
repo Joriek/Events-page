e661c9fc8665447ecb5715c8a29cf8524f763a09
Joriek Rogge <Rogge.j@telenet.be>
2014-04-28 10:00:13 +0200

    better details of the events

--- antwoord.php
+++ antwoord.php
@@ -1,32 +1,29 @@
 <?php 
-	include_once("lib/html.php");
-	include("evenementen.php");
+include_once("lib/html.php");
+include_once("lib/event.php");
+include_once("openDB.php");
 
-	$event =  null;
-	$content = "";
+$event = $_GET["event"];
 
-$id = $_GET["id"] ;
-		foreach ($events as $ev) { 
-			if ($id == $ev->id ) {
-				$event = $ev;
-			}
-		}
-
-		$content .= new Div(
-
-				new Heading($event->title) .
-				new Heading($event->description) .
-				new Heading($event->date) . 
-				new Heading($event->time) .
-				new Heading($event->website) . 
-				new Heading($event->email)
-				);
-
- ?>
-
+$STH->setFetchMode(PDO::FETCH_ASSOC);
+while($row = $STH->fetch()){
+	if($row['id']==$_GET["event"]){
+		$content =	new Heading($row['Name'],1)."\n".
+					new Paragraph($row['Discription'])."\n".
+					new Image($row['Image'],$row['image_alt'])."\n".
+					new Paragraph($row['Date']." ".$row['Time'])."\n".
+					new Link($row['Link'])."\n".
+					new Paragraph($row['Email'])."\n".
+					new Link("Back to events",array("href"=>"index.php","class"=>"btn btn-primary btn-lg active","role"=>"button")).
+					new Link("Edit Event",array("href"=>"editevent.php","class"=>"btn btn-primary btn-lg active","role"=>"button"));
+	}
+}
+?>
  <!DOCTYPE html>
  <html>
  <head>
+		 <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="css/signup.css">
  	<title></title>
  </head>
  <body>
