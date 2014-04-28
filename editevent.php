214846699fb68c74249308f7a64feac7c8306669
Joriek Rogge <Rogge.j@telenet.be>
2014-04-28 10:05:04 +0200

    added file for future code to edit the events

---
+++ editevent.php
@@ -0,0 +1,20 @@
+<?php 
+include_once("lib/html.php");
+include_once("lib/event.php");
+include_once("openDB.php");
+
+$event = $_GET["event"];
+$content = "";
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+  <?= $content ?>
+</body>
+</html>
