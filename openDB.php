83ecd5a09608964edb818fbbc23f61e07fab383c
Joriek Rogge <Rogge.j@telenet.be>
2014-05-31 12:03:43 +0200

    works

--- openDB.php
+++ openDB.php
@@ -5,7 +5,7 @@ $pass = "";
 $dbname = "oefening_events";
 $DBH=new PDO("mysql:host=$host;dbname=$dbname",$user,$pass);
 
-$STH = $DBH->query('SELECT id,Name,Date,Time,Link,Email,Image,Discription,image_alt from events');
-$STH2 = $DBH->prepare("INSERT INTO events(Name,Date,Time,Link,Email,Image,image_alt,Discription)
-			value(:Name,:Date,:Time,:Link,:Email,:Image,:image_alt,:Discription)");
+$STH = $DBH->query('SELECT id,Name,Date,Time,Link,Email,Image,Discription from events');
+$STH2 = $DBH->prepare("INSERT INTO events(Name,Date,Time,Link,Email,Image,Discription)
+			value(:Name,:Date,:Time,:Link,:Email,:Image,:Discription)");
 ?>
